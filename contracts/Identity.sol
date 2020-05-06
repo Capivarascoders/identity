0e111f1bee39eae099f7a78581bde03c2b6a1a86
Rodrigo Oliveira <rodrigo.soliveira2322@gmail.com>
2020-05-05 22:04:12 -0300

    create addValidator and addPersona

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,14 +1,120 @@
 pragma solidity 0.6.4;
-
+pragma experimental ABIEncoderV2;
 
 contract Identity {
-    string private _value;
 
-    function set(string memory value) public {
-        _value = value;
+    enum ValidationCostStrategy {
+        ForFree,
+        Charged
     }
 
-    function get() public view returns (string memory) {
-        return _value;
+    struct Validator {
+        uint validatorId;
+        address payable validatorAddress;
+        uint32 reputation;
+        ValidationCostStrategy strategy;
+        uint32 price;
+        bool exists;
+    }
+
+    struct Stamp {
+        ValidationStatus status;
+        uint whenDate;
+        uint whenBlock;
+    }
+
+    enum ValidationStatus {
+        Validated,
+        NotValidated,
+        CannotEvaluate,
+        NewData,
+        ValidationPending
+    }
+
+    struct DataToBeValidated {
+        uint price;
+        string ipfsPath;
+        Stamp[] validations;
+        ValidationStatus lastStatus;
+    }
+
+    struct Persona {
+        uint personaId;
+        address payable personaAddress;
+        mapping(string => string) personaInfos;
+        bool exists;
+    }
+
+    Validator[] private _validators;
+    mapping(uint => Validator) private _indexValidators;
+    mapping(address => Validator) private _addressValidators;
+    uint private _validatorId;
+
+    Persona[] private _personas;
+    mapping(uint => Persona) private _indexPersona;
+    mapping(address => Persona) private _addressPersona;
+    uint private _personaId;
+
+    event ValidatorAdded(
+        address validatorAddress
+    );
+
+    event PersonaAdded(
+        address personaAddress
+    );
+
+    constructor() public {
+        _personaId = 0;
+        _validatorId = 0;
+    }
+
+    function addValidator(
+        ValidationCostStrategy validationCostStrategy,
+        uint32 price
+    ) public
+    {
+        require(!_addressValidators[msg.sender].exists, 'Identity: validator already exists!');
+
+        _validatorId++;
+
+        Validator memory validator = Validator(
+            _validatorId,
+            msg.sender,
+            0,
+            validationCostStrategy,
+            price,
+            true
+        );
+
+        _validators.push(validator);
+        _indexValidators[_validatorId] = validator;
+        _addressValidators[msg.sender] = validator;
+
+        emit ValidatorAdded(msg.sender);
+    }
+
+    function addPersona(
+        string[] memory fields,
+        string[] memory values
+    ) public
+    {
+        Persona storage persona = _addressPersona[msg.sender];
+
+        require(fields.length == values.length, 'Identity: fields and values must be the same length!');
+        require(!persona.exists, 'Identity: persona already exists!');
+
+        _personaId++;
+
+        persona.personaId = _personaId;
+        persona.personaAddress = msg.sender;
+        persona.exists = true;
+
+        for(uint i = 0; i < fields.length; i++){
+            string memory field = fields[i];
+            require(bytes(persona.personaInfos[field]).length == 0, 'Identity: field already exists!');
+            persona.personaInfos[fields[i]] = values[i];
+        }
+
+        emit PersonaAdded(msg.sender);
     }
 }
--- test/identity.test.js
+++ test/identity.test.js
@@ -1,10 +1,12 @@
-const Assert = require("truffle-assertions");
-const Artifact = artifacts.require("../contracts/Identity");
+const Assert = require('truffle-assertions');
+const Artifact = artifacts.require('../contracts/Identity');
 
-contract("Identity", accounts => {
+contract('Identity', accounts => {
     let contractInstance;
 
     const ownerAddress = accounts[0];
+    const validator01Address = accounts[1];
+    const persona01Address = accounts[2];
 
     before(async () => {
         web3.eth.defaultAccount = ownerAddress;
@@ -14,9 +16,75 @@ contract("Identity", accounts => {
         contractInstance = await Artifact.new();
     });
 
-    describe('constructor', async () => {
-        it('test description', async () => {
+    describe('validator', async () => {
+        it('addValidator throw if validate already exists', async () => {
+            const validationCostStrategy = 0; //ForFree
+            const price = 100;
 
+            await contractInstance.addValidator(validationCostStrategy, price, { from: validator01Address });
+
+            await Assert.reverts(
+                contractInstance.addValidator(validationCostStrategy, price, { from: validator01Address }),
+                'Identity: validator already exists!'
+            );
+        });
+
+        it('addValidator success', async () => {
+            const validationCostStrategy = 1; //Charged
+            const price = 100;
+
+            const result = await contractInstance.addValidator(validationCostStrategy, price, { from: validator01Address });
+
+            Assert.eventEmitted(
+                result,
+                'ValidatorAdded',
+                event => event.validatorAddress == validator01Address);
+        });
+    });
+
+    describe('persona', async () => {
+        it('addPersona throw if fields and values have different length', async () => {
+            const fields = ['nome'];
+            const values = ['persona1', 'persona1@email.com'];
+
+            await Assert.reverts(
+                contractInstance.addPersona(fields, values, { from: persona01Address }),
+                'Identity: fields and values must be the same length!'
+            );
+        });
+
+        it('addPersona throw if persona already exists', async () => {
+            const fields = ['nome'];
+            const values = ['persona1'];
+
+            await contractInstance.addPersona(fields, values, { from: persona01Address });
+
+            await Assert.reverts(
+                contractInstance.addPersona(fields, values, { from: persona01Address }),
+                'Identity: persona already exists!'
+            );
+        });
+
+        it('addPersona throw if persona field already exists', async () => {
+            const fields = ['nome', 'nome'];
+            const values = ['persona1', 'persona2'];
+
+            await Assert.reverts(
+                contractInstance.addPersona(fields, values, { from: persona01Address }),
+                'Identity: field already exists!'
+            );
+        });
+
+        it('addPersona success', async () => {
+            const fields = ['nome', 'email'];
+            const values = ['persona1', 'persona1@email.com'];
+
+            const result = await contractInstance.addPersona(fields, values, { from: persona01Address });
+
+            Assert.eventEmitted(
+                result,
+                'PersonaAdded',
+                event => event.personaAddress == persona01Address);
         });
     });
 });
