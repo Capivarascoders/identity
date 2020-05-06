d05b3e3ba2c5f654552b95acd5c3e3841481db27
Rodrigo Oliveira <rodrigo.soliveira2322@gmail.com>
2020-05-06 00:01:51 -0300

    cretae askToValidate

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,15 +1,12 @@
 pragma solidity 0.6.4;
 pragma experimental ABIEncoderV2;
 
-contract Identity {
 
-    enum ValidationCostStrategy {
-        ForFree,
-        Charged
-    }
+contract Identity {
+    enum ValidationCostStrategy {ForFree, Charged}
 
     struct Validator {
-        uint validatorId;
+        uint256 validatorId;
         address payable validatorAddress;
         uint32 reputation;
         ValidationCostStrategy strategy;
@@ -19,41 +16,49 @@ contract Identity {
 
     struct Stamp {
         ValidationStatus status;
-        uint whenDate;
-        uint whenBlock;
+        uint256 whenDate;
+        address addressValidator;
     }
 
     enum ValidationStatus {
         Validated,
         NotValidated,
         CannotEvaluate,
-        NewData,
         ValidationPending
     }
 
     struct DataToBeValidated {
-        uint price;
+        uint idDataToBeValidated;
+        uint256 idPersona;
+        uint256 price;
+        string field;
         string ipfsPath;
         Stamp[] validations;
         ValidationStatus lastStatus;
     }
 
     struct Persona {
-        uint personaId;
+        uint256 personaId;
         address payable personaAddress;
         mapping(string => string) personaInfos;
         bool exists;
     }
 
     Validator[] private _validators;
-    mapping(uint => Validator) private _indexValidators;
+    mapping(uint256 => Validator) private _indexValidators;
     mapping(address => Validator) private _addressValidators;
-    uint private _validatorId;
+    uint256 private _validatorId;
 
     Persona[] private _personas;
-    mapping(uint => Persona) private _indexPersona;
+    mapping(uint256 => Persona) private _indexPersona;
     mapping(address => Persona) private _addressPersona;
-    uint private _personaId;
+    uint256 private _personaId;
+
+    DataToBeValidated[] private _validations;
+    mapping(uint256 => DataToBeValidated) private _indexDataToBeValidate;
+    mapping(uint256 => DataToBeValidated[]) private _personaIdDataToBeValidate;
+    mapping(uint256 => DataToBeValidated[]) private _validatorIdDataToBeValidate;
+    uint256 private _dataToBeValidatedId;
 
     event ValidatorAdded(
         address validatorAddress
@@ -63,17 +68,30 @@ contract Identity {
         address personaAddress
     );
 
+    event InfoAdded(
+        address personaAddress
+    );
+
+    event ValidateAdded(
+        address personaAddress,
+        address validateAddress,
+        string field
+    );
+
     constructor() public {
         _personaId = 0;
         _validatorId = 0;
+        _dataToBeValidatedId = 0;
     }
 
     function addValidator(
         ValidationCostStrategy validationCostStrategy,
         uint32 price
-    ) public
-    {
-        require(!_addressValidators[msg.sender].exists, 'Identity: validator already exists!');
+    ) public {
+        require(
+            !_addressValidators[msg.sender].exists,
+            'Identity: validator already exists!'
+        );
 
         _validatorId++;
 
@@ -100,8 +118,15 @@ contract Identity {
     {
         Persona storage persona = _addressPersona[msg.sender];
 
-        require(fields.length == values.length, 'Identity: fields and values must be the same length!');
-        require(!persona.exists, 'Identity: persona already exists!');
+        require(
+            fields.length == values.length,
+            'Identity: fields and values must be the same length!'
+        );
+
+        require(
+            !persona.exists,
+            'Identity: persona already exists!'
+        );
 
         _personaId++;
 
@@ -109,12 +134,106 @@ contract Identity {
         persona.personaAddress = msg.sender;
         persona.exists = true;
 
-        for(uint i = 0; i < fields.length; i++){
+        for (uint256 i = 0; i < fields.length; i++) {
             string memory field = fields[i];
-            require(bytes(persona.personaInfos[field]).length == 0, 'Identity: field already exists!');
+            require(
+                bytes(persona.personaInfos[field]).length == 0,
+                'Identity: field already exists!'
+            );
             persona.personaInfos[fields[i]] = values[i];
         }
 
         emit PersonaAdded(msg.sender);
+    }
+
+    function addInfos(
+        string[] memory fields,
+        string[] memory values
+    ) public
+    {
+        Persona storage persona = _addressPersona[msg.sender];
+
+        require(
+            fields.length == values.length,
+            'Identity: fields and values must be the same length!'
+        );
+
+        require(
+            persona.exists,
+            'Identity: persona not exists!'
+        );
+
+        for (uint256 i = 0; i < fields.length; i++) {
+            string memory field = fields[i];
+            require(
+                bytes(persona.personaInfos[field]).length == 0,
+                'Identity: field already exists!'
+            );
+            persona.personaInfos[fields[i]] = values[i];
+        }
+
+        emit InfoAdded(msg.sender);
+    }
+
+    function askToValidate(
+        address validatorAddress,
+        string memory field,
+        string memory ipfsHash
+    ) public
+      payable
+    {
+        Validator memory validator = _addressValidators[validatorAddress];
+        require(
+            validator.exists,
+            'Identity: validator not exists!'
+        );
+
+        Persona storage persona = _addressPersona[msg.sender];
+        require(
+            persona.exists,
+            'Identity: persona not exists!'
+        );
+
+        if (validator.strategy == ValidationCostStrategy.Charged) {
+            require(
+                msg.value == validator.price,
+                'Identity: sended value is not the validator price!'
+            );
+            validator.validatorAddress.transfer(msg.value);
+        }
+
+        require(
+            bytes(persona.personaInfos[field]).length == 0,
+            'Identity: field not exists!'
+        );
+
+        _dataToBeValidatedId++;
+
+        DataToBeValidated storage data = _indexDataToBeValidate[_dataToBeValidatedId];
+        data.idDataToBeValidated = _dataToBeValidatedId;
+        data.idPersona = persona.personaId;
+        data.price = msg.value;
+        data.field = field;
+        data.ipfsPath = ipfsHash;
+        data.validations[0] = Stamp(ValidationStatus.ValidationPending, now, validatorAddress);
+        data.lastStatus = ValidationStatus.ValidationPending;
+
+        _validations.push(data);
+        _indexDataToBeValidate[_dataToBeValidatedId] = data;
+        _personaIdDataToBeValidate[persona.personaId].push(data);
+        _validatorIdDataToBeValidate[validator.validatorId].push(data);
+
+        emit ValidateAdded(
+            persona.personaAddress,
+            validator.validatorAddress,
+            field
+        );
+    }
+
+    function validate(
+
+    ) public
+    {
+
     }
 }
--- test/identity.test.js
+++ test/identity.test.js
@@ -87,4 +87,69 @@ contract('Identity', accounts => {
                 event => event.personaAddress == persona01Address);
         });
     });
+
+    describe('info', async () => {
+        it('addInfos throw if fields and values have different length', async () => {
+            const fields = ['nome'];
+            const values = ['persona1', 'persona1@email.com'];
+
+            await Assert.reverts(
+                contractInstance.addInfos(fields, values, { from: persona01Address }),
+                'Identity: fields and values must be the same length!'
+            );
+        });
+
+        it('addInfos throw if persona not exists', async () => {
+            const fields = ['nome'];
+            const values = ['persona1'];
+
+            await Assert.reverts(
+                contractInstance.addInfos(fields, values, { from: persona01Address }),
+                'Identity: persona not exists!'
+            );
+        });
+
+        it('addInfos throw if persona field already exists', async () => {
+            const fields = ['nome'];
+            const values = ['persona1'];
+
+            const newFields = ['nome'];
+            const newValues = ['persona'];
+
+            await contractInstance.addPersona(fields, values, { from: persona01Address });
+
+            await Assert.reverts(
+                contractInstance.addInfos(newFields, newValues, { from: persona01Address }),
+                'Identity: field already exists!'
+            );
+        });
+
+        it('addInfos success', async () => {
+            const fields = ['nome', 'email'];
+            const values = ['persona1', 'persona1@email.com'];
+
+            const newFields = ['idade'];
+            const newValues = ['18'];
+
+            await contractInstance.addPersona(fields, values, { from: persona01Address });
+            const result = await contractInstance.addInfos(newFields, newValues, { from: persona01Address });
+
+            Assert.eventEmitted(
+                result,
+                'InfoAdded',
+                event => event.personaAddress == persona01Address);
+        });
+    });
+
+    describe('validations', async () => {
+        it('askToValidate throw if validator not exists', async () => {
+            const field = 'nome';
+            const ipfsHash = 'https://ipfs.infura.io/ipfs/Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a';
+
+            await Assert.reverts(
+                contractInstance.askToValidate(validator01Address, field, ipfsHash, { from: persona01Address }),
+                'Identity: validator not exists!'
+            );
+        });
+    });
 });
